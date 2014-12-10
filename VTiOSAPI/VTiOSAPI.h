4cf777b8e1aaf3c399e8930f6676854bec565a43
Muhammad Anis <muhammad.anis@veritrans.co.id>
2014-12-10 16:51:44 +0700

    Initial Commit

---
+++ VTiOSAPI/VTiOSAPI.h
@@ -0,0 +1,13 @@
+//
+//  VTiOSAPI.h
+//  VTiOSAPI
+//
+//  Created by Muhammad Anis on 12/10/14.
+//  Copyright (c) 2014 Veritrans Indonesia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VTiOSAPI : NSObject
+
+@end
---
+++ VTiOSAPI/VTiOSAPI.m
@@ -0,0 +1,13 @@
+//
+//  VTiOSAPI.m
+//  VTiOSAPI
+//
+//  Created by Muhammad Anis on 12/10/14.
+//  Copyright (c) 2014 Veritrans Indonesia. All rights reserved.
+//
+
+#import "VTiOSAPI.h"
+
+@implementation VTiOSAPI
+
+@end
