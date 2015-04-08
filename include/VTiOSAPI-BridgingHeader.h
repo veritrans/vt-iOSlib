86c6553a9f48dc475b64f1e83b82f724aa387e59
Muhammad Anis <muhammad.anis@veritrans.co.id>
2015-04-08 14:09:08 +0700

    add support for arm64 and x86_64

---
+++ include/VTiOSAPI-BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  VTiOSAPI-BridgingHeader.h
+//  VTiOSAPI
+//
+//  Created by Muhammad Anis on 12/11/14.
+//  Copyright (c) 2014 Veritrans Indonesia. All rights reserved.
+//
+
+#ifndef __VTiOS_API_BRIDGING_HEADER_H__
+#define __VTiOS_API_BRIDGING_HEADER_H__
+
+#import "VTDirect.h"
+#import "VTConfig.h"
+
+#endif
